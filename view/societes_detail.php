d499a17e561ba849209d957fa1b87ef2b7c61636
ericLavachery <eric.lavachery@gmail.com>
2018-08-29 16:26:49 +0200

    detail société

--- view/societes_detail.php
+++ view/societes_detail.php
@@ -12,12 +12,14 @@
         <tr>
             <td class="tabtitle">Société</td>
             <td class="tabtitle">Type</td>
+            <td class="tabtitle">Adresse</td>
             <td class="tabtitle">Téléphone</td>
             <td class="tabtitle">TVA</td>
         </tr>
         <tr>
             <td class="tabrow"><a href="index.php?page=lesboites_detail&id=<?=$societe['id']?>"><?=$societe['nom']?></a></td>
             <td class="tabrow"><?=$societe['type']?></td>
+            <td class="tabrow"><?=$societe['adresse']?> | <?=$societe['pays']?></td>
             <td class="tabrow"><?=$societe['telephone']?></td>
             <td class="tabrow"><?=$societe['tva']?></td>
         </tr>
