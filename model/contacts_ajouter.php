c8a018e8f2ff2bb4d7def2233dbdd5e77941d6c3
ericLavachery <eric.lavachery@gmail.com>
2018-08-30 13:03:44 +0200

    formulaire de combat

--- model/contacts_ajouter.php
+++ model/contacts_ajouter.php
@@ -1,6 +1,13 @@
 <?php
-require_once('connect.php');
+// require_once('connect.php');
 $titre = "Nouveau contact";
+
+$lname = '';
+$fname = '';
+$phone = '';
+$email = '';
+$val_email = '';
+$societe = '';
 
 $query = "SELECT nom,id
 FROM societe
@@ -10,24 +17,22 @@ $stmt = $db->query($query);
 $societes = $stmt->fetchAll();
 
 // Si utilisation du bouton "Ajouter dans le contact"
-    if(isset($_POST['btn'])){
-      var_dump($_POST);
-        $lname = filter_var($_POST['nom'], FILTER_SANITIZE_STRING);
-        $fname = filter_var($_POST['prenom'], FILTER_SANITIZE_STRING);
-        $phone = filter_var($_POST['telephone'], FILTER_SANITIZE_NUMBER_INT);
-        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-        $val_email = filter_var($email, FILTER_VALIDATE_EMAIL);
-        $societe = $_POST['societe'];
-        $btn_delete = isset($_POST['btn_delete']);
+if(isset($_POST['btn'])){
+    $lname = filter_var($_POST['nom'], FILTER_SANITIZE_STRING);
+    $fname = filter_var($_POST['prenom'], FILTER_SANITIZE_STRING);
+    $phone = filter_var($_POST['telephone'], FILTER_SANITIZE_NUMBER_INT);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $val_email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    $societe = $_POST['societe'];
+    $btn_delete = isset($_POST['btn_delete']);
 
     if(!empty($lname) && !empty($fname) && !empty($phone) && !empty($val_email)) {
-            //insérer nouvelles données 
-            $add_value = $db->query
-            ('INSERT INTO personne(nom, prenom, telephone, email, societe_id) VALUES("'.$lname.'", "'.$fname.'", "'.$phone.'", "'.$email.'", "'.$societe.'")');
-    
-        }else{
-            echo "ERROR";
-        }
-    
-        echo "<a href='../index.php?page=lesgens_ajouter'> Retour </a>" ;
-} ?>
+        //insérer nouvelles données
+        $add_value = $db->query
+        ('INSERT INTO personne(nom, prenom, telephone, email, societe_id) VALUES("'.$lname.'", "'.$fname.'", "'.$phone.'", "'.$email.'", "'.$societe.'")');
+
+    }else{
+
+    }
+}
+?>
--- view/contacts_ajouter.php
+++ view/contacts_ajouter.php
@@ -8,44 +8,37 @@
 <body>
     <h1><?= $titre ?></h1>
 
-    	<form action="model/contacts_ajouter.php" method="post">
-
-		<div>
-			<label for="nom">Nom : </label>
-			<input type="text" name="nom">
-		</div>
-
-		<div>
-			<label for="prenom">Prenom : </label>
-			<input type="text" name="prenom">
-		</div>
-
-		<div>
-			<label for="telephone">Numéro de téléphone : </label>
-			<input type="text" name="telephone">
-		</div>
-
-		<div>
-			<label for="bas"> email : </label>
-			<input type="email" name="email">
-		</div>
-
-		<div>
-			<label for="societe"> Société : </label>
-			<select name="societe">
-					<option></option>
-				<?php foreach ($societes as $key => $value){?>
-        			<option value="<?= $value['id']?>"><?= $value['nom']?></option>
-        		<?php } ?>
-			</select>
-		</div>
-
-		<div>
-			<button type="submit" name="btn"> Ajouter le contact</button>
-		</div>
-		
-	</form>
+    <form action="" method="post">
+        <div>
+            <label for="prenom">Prenom : </label>
+            <input type="text" name="prenom" value="<?= $fname ?>" required>
+        </div>
+        <div>
+            <label for="nom">Nom : </label>
+            <input type="text" name="nom" value="<?= $lname ?>" required>
+        </div>
+        <div>
+            <label for="telephone">Téléphone : </label>
+            <input type="text" name="telephone" value="<?= $phone ?>" required>
+        </div>
+        <div>
+            <label for="bas"> email : </label>
+            <input type="email" name="email" value="<?= $email ?>" required>
+        </div>
+        <div>
+            <label for="societe"> Société : </label>
+            <select name="societe" required>
+                <option value="" selected></option>
+                <?php foreach ($societes as $key => $value){?>
+                    <option value="<?= $value['id']?>"<?php if ($societe == $value['id']) {echo ' selected';}; ?>><?= $value['nom']?></option>
+                <?php } ?>
+            </select>
+        </div>
+        <div>
+            <label for="btn"></label>
+            <button type="submit" name="btn"> Ajouter le contact</button>
+        </div>
+    </form>
 
 </body>
 </html>
-
