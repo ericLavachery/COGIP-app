51804ae3116b9ddb5234b2ba6b58e5e252e5214b
ericLavachery <eric.lavachery@gmail.com>
2018-08-30 13:45:41 +0200

    des trucs

--- view/contacts_ajouter.php
+++ view/contacts_ajouter.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="view/css/style.css">
-    <title></title>
+    <title><?= $titre ?></title>
 </head>
 <body>
     <h1><?= $titre ?></h1>
