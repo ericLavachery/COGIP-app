0e16dfd8dd3141e9b0eb72f91a922ecbf7dcc7e1
ericLavachery <eric.lavachery@gmail.com>
2018-08-31 14:53:38 +0200

    changement personne et société dans facture

--- view/factures_modifier.php
+++ view/factures_modifier.php
@@ -15,18 +15,18 @@
         </div>
         <div>
             <label for="date_facturation">Date : </label>
-            <input type="text" name="date_facturation" value="<?= $date_facturation ?>" required>
+            <input type="text" name="date_facturation" value="<?= $date_facturation ?>" required placeholder="AAAA-MM-JJ">
         </div>
         <div>
             <label for="motif_prestation">Prestation : </label>
-            <input type="text" name="motif_prestation" value="<?= $motif_prestation ?>" required>
+            <textarea name="motif_prestation" rows="3" cols="41" required><?= $motif_prestation ?></textarea>
         </div>
         <div>
             <label for="personne_id"> Contact : </label>
             <select name="personne_id" required>
                 <option value="" selected></option>
                 <?php foreach ($contacts as $key => $value){?>
-                    <option value="<?= $value['id']?>"<?php if ($personne_id == $value['id']) {echo ' selected';}; ?>><?= $value['prenom']?> <?= $value['nom']?></option>
+                    <option value="<?= $value['id']?>"<?php if ($personne_id == $value['id']) {echo ' selected';}; ?>><?= $value['nom_societe']?> : <?= $value['prenom']?> <?= $value['nom']?></option>
                 <?php } ?>
             </select>
         </div>
