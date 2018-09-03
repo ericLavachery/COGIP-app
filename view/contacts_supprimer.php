67a9885fb26824a3dc40e4bc05afc4fde482ac07
Geraldine <winand.geraldine@hotmail.com>
2018-09-03 14:31:08 +0200

    work in progress

--- view/contacts_supprimer.php
+++ view/contacts_supprimer.php
@@ -1,21 +1,18 @@
-
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="view/css/style.css">
     <title>COGIP : <?= $titre ?></title>
 </head>
-<body><?php include '_navig.php' ?>
+<body>
+    <?php include '_navig.php' ?>
     <h1><?= $titre ?></h1>
 
-<h3><a href="index.php?page=lesboites_detail&id=<?=$personnes['id_societe']?>"><?=$personnes['nom_societe']?></a></h3>
+<h3><?=$personnes['nom_societe']?></h3>
 
     Tél : <?=$personnes['telephone']?><br>
     Email : <?=$personnes['email']?><br>
-
-    <a href="index.php?page=lesgens_modifier&id=<?=$idcontact?>"><button class="form" type="button" name="button">Modifier</button></a>
-    <a href="index.php?page=lesgens_supprimer&id=<?=$idcontact?>"><button class="form" type="button" name="button">Supprimer</button></a>
 
     <h3>Factures</h3>
     <table>
@@ -24,14 +21,21 @@
             <td class="tabtitle">Date</td>
             <td class="tabtitle">Prestation</td>
         </tr>
-        <?php foreach ($factures as $key => $value){?>
+      <?php foreach ($factures as $key => $value){?>
             <tr>
-                <td class="tabrow"><a href="index.php?page=lepognon_detail&id=<?=$value['id']?>"><?=$value['numero']?></td>
+                <td class="tabrow"><?=$value['numero']?></td>
                 <td class="tabrow"><?=$value['date_facturation']?></td>
                 <td class="tabrow"><?=$value['motif_prestation']?></td>
             </tr>
         <?php } ?>
     </table>
+    <br>
+     <a href="?page=lesgens_supprimer&id=<?=$idcontact?>"><button type="button" name="button">Supprimer le contact</button></a>
+
+     <div class="rouge">
+        <?= $message ?>
+         
+     </div>
 
 </body>
 </html>
